ef56c6578bccf0af9b82ee42111dcbd69116c988
pymba <aleksey/nester.itt@gmail.com>
2015-02-25 10:41:30 +0200

    jabber plugin for grails 2.*

---
+++ JabberGrailsPlugin.groovy
@@ -0,0 +1,129 @@
+import org.codehaus.groovy.grails.commons.GrailsClassUtils
+import grails.util.Holders as CFG
+
+import org.codehaus.groovy.grails.jabber.ChatListener
+
+
+class JabberGrailsPlugin {
+    def version = 2.0
+    def dependsOn = [:]
+    def grailsVersion = "2.4 > *"
+    def author = "Aleksey Nesterenko"
+    def authorEmail = "aleksey.nester.itt@gmail.com"
+    def title = "Jabber Plugin"
+    def description = '''\
+This plugin provides the opportunity to send and receive Chat messages via the Jabber API.
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "http://grails.org/plugin/jabber"
+
+    def doWithSpring = {
+
+        boolean listenerDefined = false
+
+        application.serviceClasses?.each { service ->
+            def serviceClass = service.getClazz()
+            def exposeList = GrailsClassUtils.getStaticPropertyValue(serviceClass, 'expose')
+            if (exposeList != null && exposeList.contains('jabber') && !listenerDefined) {
+                println "adding Jabber listener for ${service.shortName} to Spring"
+
+                def method = GrailsClassUtils.getStaticPropertyValue(serviceClass, 'jabberListenerMethod')
+                if (!method)
+                    method = "onJabberMessage"
+
+		"GrailsPluginJabberListener"(org.codehaus.groovy.grails.jabber.ChatListener) {
+                    host = CFG.config.chat.host
+                    port = CFG.config.chat.port
+                    serviceName = CFG.config.chat.serviceName
+                    userName = CFG.config.chat.username
+                    password = CFG.config.chat.password
+                    listenerMethod = method
+                    targetService = ref("${service.propertyName}")
+
+                }
+                listenerDefined = true
+
+				
+            }
+        }
+        if (!listenerDefined) {
+
+            "GrailsPluginJabberListener"(org.codehaus.groovy.grails.jabber.ChatListener) {
+                    host = CFG.config.chat.host
+                    port = CFG.config.chat.port
+                    serviceName = CFG.config.chat.serviceName
+                    userName = CFG.config.chat.username
+                    password = CFG.config.chat.password
+             }
+
+        }
+
+    }
+   
+    def doWithApplicationContext = { applicationContext ->
+
+        boolean listenerStarted = false
+        application.serviceClasses?.each { service ->
+            def serviceClass = service.getClazz()
+            def exposeList = GrailsClassUtils.getStaticPropertyValue(serviceClass, 'expose')
+            if (exposeList!=null && exposeList.contains('jabber') && !listenerStarted) {
+                println "Starting Jabber listener for ${service.shortName}"
+                def listener = applicationContext.getBean("GrailsPluginJabberListener")
+                listener.listen()
+                println "Added listener ok ${listener.dump()}"
+                listenerStarted = true
+            }
+        }
+
+
+    }
+
+    def doWithWebDescriptor = { xml ->
+        // TODO Implement additions to web.xml (optional)
+    }
+
+    def sendJabberMessage = {cl, to, message ->
+    	
+    	if (to instanceof List) {
+    	    to.each { addr ->
+                cl.sendJabberMessage(addr, message)    
+            }
+        } else {
+            cl.sendJabberMessage(to, message)
+        }
+
+    }
+	                                      
+    def doWithDynamicMethods = { ctx ->
+
+        ChatListener cl = new ChatListener(host: CFG.config.chat.host,
+            serviceName: CFG.config.chat.serviceName,
+            userName: CFG.config.chat.username,
+            password: CFG.config.chat.password
+        )
+
+        def listener = applicationContext.getBean("GrailsPluginJabberListener")
+
+        application.serviceClasses?.each { service ->
+            def mc = service.metaClass          
+            mc.sendJabberMessage = sendJabberMessage.curry(listener)
+        }
+        application.controllerClasses?.each { controller ->
+            def mc = controller.metaClass
+            mc.sendJabberMessage = sendJabberMessage.curry(listener)
+    	}
+
+    }
+	
+    def onChange = { event ->
+        // TODO Implement code that is executed when any artefact that this plugin is
+        // watching is modified and reloaded. The event contains: event.source,
+        // event.application, event.manager, event.ctx, and event.plugin.
+    }
+
+    def onConfigChange = { event ->
+        // TODO Implement code that is executed when the project configuration changes.
+        // The event is the same as for 'onChange'.
+    }
+}
